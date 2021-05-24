4989209bdea829826fc669a8851329f361938368
Vittakor <vittakor@gmail.com>
2021-05-24 22:34:35 +0700

    update

--- src/test/java/definitions/StoreStepdefs.java
+++ src/test/java/definitions/StoreStepdefs.java
@@ -26,7 +26,7 @@ public class StoreStepdefs {
 
     @When("I decide to wait for {int} seconds")
     public void iDecideToWaitForSeconds(int time) throws Exception {
-        Thread.sleep(time * 1000);
+        Thread.sleep(time * 1000L);
     }
 
     @When("I click on the search button")
@@ -34,22 +34,177 @@ public class StoreStepdefs {
         getDriver().findElement(By.name("submit_search")).click();
     }
 
-    @When("I wait for element {string} to be displayed")
-    public void iWaitForElementToBeDisplayed(String xpath) {
-        getDriver().findElement(By.xpath(xpath)).isDisplayed();
-    }
-
     @Then("The error message {string} should appears")
     public void theErrorMessageShouldAppears(String message) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+ message +"')]")).isDisplayed();
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + message + "')]")).isDisplayed();
     }
 
-    @Then("The search results counter should displayed {string} results")
-    public void theSearchResultsCounterShouldDisplayedResults(String counter) {
-        assertThat(getDriver().findElement(By.xpath("//span[@class='heading-counter']")).getText()).contains(""+counter+" results have been found.");
+    @Then("The search results counter should displayed {int} results")
+    public void theSearchResultsCounterShouldDisplayedResults(int counter) {
+        if (counter == 1) {
+            assertThat(getDriver().findElement(By.xpath("//span[@class='heading-counter']")).getText()).contains("" + counter + " result has been found.");
+        } else if (counter >= 2) {
+            assertThat(getDriver().findElement(By.xpath("//span[@class='heading-counter']")).getText()).contains("" + counter + " results have been found.");
+        } else if (counter == 0) {
+            assertThat(getDriver().findElement(By.xpath("//span[@class='heading-counter']")).getText()).contains("" + counter + " results have been found.");
+        }
     }
 
+    @Then("The message Showing ... of {int} items appears")
+    public void theMessageShowingOfItemsAppears(int results) {
+        if (results == 1) {
+            assertThat(getDriver().findElement(By.xpath("//*[@class='product-count']")).getText()).contains("of " + results + " item");
+        } else if (results > 1) {
+            assertThat(getDriver().findElement(By.xpath("//*[@class='product-count']")).getText()).contains("of " + results + " items");
+        }
+    }
 
+    @When("I wait until the page fully load")
+    public void iWaitUntilThePageFullyLoad() {
+        assertThat(getDriver().findElement(By.xpath("//div[@id='columns']")).isDisplayed()).isTrue();
+    }
+
+    @When("I click Sign in button")
+    public void iClickSignInButton() {
+        getDriver().findElement(By.xpath("//*[@class='login']")).click();
+    }
+
+    @When("I type {string} in the email field")
+    public void iTypeInTheEmailField(String email) {
+        getDriver().findElement(By.id("email_create")).sendKeys(email);
+    }
+
+    @When("I try to create an account")
+    public void iTryToCreateAnAccount() {
+        getDriver().findElement(By.id("SubmitCreate")).click();
+    }
+
+    @Then("I get {string} error message")
+    public void iGetErrorMessage(String message) {
+        assertThat(getDriver().findElement(By.xpath("//div[@id='create_account_error']//*[contains(text(),'" + message + "')]")).getText()).contains(message);
+    }
+
+    @When("I try to submit the form")
+    public void iTryToSubmitTheForm() {
+        getDriver().findElement(By.id("submitAccount")).click();
+    }
+
+    @When("I fill out password field as {string}")
+    public void iFillOutPasswordFieldAs(String password) {
+        getDriver().findElement(By.id("passwd")).sendKeys(password);
+    }
+
+    @Then("I assert actual error message with {string}")
+    public void iAssertActualErrorMessageWith(String errorMs) {
+        String actualRes = getDriver().findElement(By.xpath("//*[@class='alert alert-danger']//*[contains(text(),'passwd')]/..")).getText();
+        assertThat(actualRes).containsIgnoringCase(errorMs);
+    }
+
+    @Then("no password errors appear")
+    public void noPasswordErrorsAppear() {
+        String result = getDriver().findElement(By.xpath("//*[@class='alert alert-danger']")).getText();
+        assertThat(result).doesNotContain("passwd");
+    }
+
+    @When("I clear the form")
+    public void iClearTheForm() {
+        getDriver().findElement(By.id("email")).clear();
+        getDriver().findElement(By.id("alias")).clear();
+        getDriver().findElement(By.id("id_country")).click();
+        getDriver().findElement(By.xpath("//select[@id='id_country']/option[1]")).click();
+    }
+
+    @When("I provide valid input {string} to field {string}")
+    public void iProvideValidInputToField(String input, String field) {
+        getDriver().findElement(By.id(field)).sendKeys(input);
+    }
+
+    @Then("Error message {string} should not be displayed")
+    public void errorMessageShouldNotBeDisplayed(String message) {
+        String result = getDriver().findElement(By.xpath("//*[@class='alert alert-danger']")).getText();
+        assertThat(result).doesNotContain(message);
+    }
+
+    @Then("required fields trigger error messages")
+    public void requiredFieldsTriggerErrorMessages() {
+        String result = getDriver().findElement(By.xpath("//*[@class='alert alert-danger']")).getText();
+        assertThat(result).containsIgnoringCase("You must register at least one phone number.");
+        assertThat(result).containsIgnoringCase("lastname is required.");
+        assertThat(result).containsIgnoringCase("firstname is required.");
+        assertThat(result).containsIgnoringCase("passwd is required.");
+        assertThat(result).containsIgnoringCase("email is required.");
+        assertThat(result).containsIgnoringCase("alias is required.");
+        assertThat(result).containsIgnoringCase("address1 is required.");
+        assertThat(result).containsIgnoringCase("city is required.");
+        assertThat(result).containsIgnoringCase("Country cannot be loaded with address->id_country");
+        assertThat(result).containsIgnoringCase("Country is invalid");
+    }
+
+    @When("I fill out country")
+    public void iFillOutCountry() {
+        getDriver().findElement(By.id("id_country")).click();
+        getDriver().findElement(By.xpath("//select[@id='id_country']/option[2]")).click();
+    }
+
+    @When("I select a state")
+    public void iSelectAState() {
+        getDriver().findElement(By.id("id_state")).click();
+        getDriver().findElement(By.xpath("//select[@id='id_state']//option[1]")).click();
+    }
+
+    @Then("The country and zip related messages appears")
+    public void theCountryAndZipRelatedMessagesAppears() {
+        String result = getDriver().findElement(By.xpath("//*[@class='alert alert-danger']")).getText();
+        assertThat(result).containsIgnoringCase("This country requires you to choose a State.");
+        assertThat(result).containsIgnoringCase("It must follow this format: 00000");
+    }
+
+    @When("I fill out the form with valid info")
+    public void iFillOutTheFormWithValidInfo() {
+        getDriver().findElement(By.id("customer_firstname")).sendKeys("First");
+        getDriver().findElement(By.id("customer_lastname")).sendKeys("Last");
+        getDriver().findElement(By.id("passwd")).sendKeys("12345");
+        getDriver().findElement(By.id("address1")).sendKeys("123 Main ave.");
+        getDriver().findElement(By.id("city")).sendKeys("Austin");
+        getDriver().findElement(By.id("phone_mobile")).sendKeys("87654321234");
+        getDriver().findElement(By.id("postcode")).sendKeys("54321");
+        getDriver().findElement(By.id("id_state")).click();
+        getDriver().findElement(By.xpath("//select[@id='id_state']//option[2]")).click();
+    }
+
+    @When("I click on `My address` button")
+    public void iClickOnMyAddressButton() {
+        getDriver().findElement(By.xpath("//*[@title='Addresses']")).click();
+    }
+
+    @Then("I verify info in `My address` tab")
+    public void iVerifyInfoInMyAddressTab() {
+        String address = getDriver().findElement(By.xpath("//*[@class='last_item item box']")).getText();
+        assertThat(address).containsIgnoringCase("My address");
+        assertThat(address).containsIgnoringCase("First");
+        assertThat(address).containsIgnoringCase("Last");
+        assertThat(address).containsIgnoringCase("54321");
+        assertThat(address).containsIgnoringCase("Austin");
+        assertThat(address).containsIgnoringCase("87654321234");
+        assertThat(address).containsIgnoringCase("United States");
+        assertThat(address).containsIgnoringCase("123 Main ave.");
+        assertThat(address).containsIgnoringCase("Alabama");
+    }
+
+    @When("I go back to my account")
+    public void iGoBackToMyAccount() {
+        getDriver().navigate().back();
+    }
+
+    @When("I click on `My personal info` button")
+    public void iClickOnMyPersonalInfoButton() {
+        getDriver().findElement(By.xpath("//*[@title='Information']")).click();
+    }
+
+    @Then("I verify info inside `My personal info` tab")
+    public void iVerifyInfoInsideMyPersonalInfoTab() {
+        assertThat(getDriver().findElement(By.id("firstname")).getAttribute("value")).containsIgnoringCase("First");
+        assertThat(getDriver().findElement(By.id("lastname")).getAttribute("value")).containsIgnoringCase("Last");
+        assertThat(getDriver().findElement(By.id("email")).getAttribute("value")).containsIgnoringCase("mail.ru");
+    }
 }
-
-
--- src/test/resources/features/Store.feature
+++ src/test/resources/features/Store.feature
@@ -5,52 +5,100 @@ Feature: store
     Given I go to store web site
     Then  I verify page title as "My Store"
 
-
   @store1
   Scenario Outline: Fill out and validate “Search” field.
     When I type <request> in the search field
     When I click on the search button
-    When I wait for element "//div[@id='center_column']" to be displayed
+    When I decide to wait for 2 seconds
     Then The error message <message> should appears
     Then The search results counter should displayed <counter> results
+    Then The message Showing ... of <results> items appears
+    Examples:
+      | request  | message                                 | counter | results |
+      | ""       | "Please enter a search keyword"         | 0       | 0       |
+      | "blouse" | ""                                      | 1       | 1       |
+      | "short"  | ""                                      | 4       | 4       |
+      | "car"    | "No results were found for your search" | 0       | 0       |
+
+  @store2
+  Scenario Outline: Validate ”Create an account” behavior
+    When I click Sign in button
+    When I type <email> in the email field
+    When I try to create an account
+    When I decide to wait for 2 seconds
+    Then I get <message> error message
+    Examples:
+      | email           | message                                                            |
+      | ""              | "Invalid email address"                                            |
+      | " "             | "Invalid email address"                                            |
+      | "@"             | "Invalid email address"                                            |
+      | "a@"            | "Invalid email address"                                            |
+      | "a@a"           | "Invalid email address"                                            |
+      | "a@a."          | "Invalid email address"                                            |
+      | "@."            | "Invalid email address"                                            |
+      | "test"          | "Invalid email address"                                            |
+      | ".com"          | "Invalid email address"                                            |
+      | "test@test.com" | "An account using this email address has already been registered." |
+
+
+  @store3
+  Scenario: Fill out and validate “Password” set of fields
+    When I click Sign in button
+    When I type "carqsikgt@mail.ru" in the email field
+    When I try to create an account
+    When I wait for element with xpath "//button[@id='submitAccount']" to be present
+    When I try to submit the form
+    Then I assert actual error message with "passwd is required."
+    When I fill out password field as "1234"
+    When I try to submit the form
+    Then I assert actual error message with "passwd is invalid."
+    Then I fill out password field as "12345"
+    When I try to submit the form
+    Then no password errors appear
+
+  @store4
+  Scenario Outline: Validate Required fields behavior
+    When I click Sign in button
+    When I type "aarsikgt@mail.ru" in the email field
+    When I try to create an account
+    When I wait for element with xpath "//button[@id='submitAccount']" to be present
+    When I clear the form
+    When I try to submit the form
+    Then required fields trigger error messages
+    When I fill out country
+    When I try to submit the form
+    Then The country and zip related messages appears
+    When I select a state
+    When I provide valid input <input> to field <field>
+    When I try to submit the form
+    Then Error message <message> should not be displayed
 
     Examples:
-      | request | message                                 | counter |
-      | ""      | "Please enter a search keyword"         | "0"     |
-      | "short" | "Showing"                               | "4"     |
-      | "car"   | "No results were found for your search" | "0"     |
+      | input              | field                | message                                        |
+      | "First"            | "customer_firstname" | "firstname is required."                       |
+      | "Last"             | "customer_lastname"  | "lastname is required."                        |
+      | "qarsikgt@mail.ru" | "email"              | "email is required."                           |
+      | "12345"            | "passwd"             | "passwd is required."                          |
+      | "123 Main ave."    | "address1"           | "address1 is required."                        |
+      | "Austin"           | "city"               | "city is required."                            |
+      | "8765432112"       | "phone_mobile"       | "You must register at least one phone number." |
+      | "My address"       | "alias"              | "alias is required."                           |
+      | "54321"            | "postcode"           | "It must follow this format: 00000"            |
+      | ""                 | ""                   | "This country requires you to choose a State." |
 
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#
-#
-#Test Case 1: Fill out and validate “Search” field.
-#Validate empty input
-#Validate input of item existing on the page
-#Validate input of item nonexistent on the page
-#
-#Test Case 2: Validate” Create an account” behavior
-#Validate that email field accepts only valid email addresses.
-#Validate that email field does not accept email addresses that have already been registered
-#
-#Test Case 3:
-#Fill out and validate “Password” set of fields.
-#Validate 5 characters minimum12345
-#
-#Test Case 4: Validate Required fields behavior
-#
-#Test Case 5: Submit the form and verify the data on My Account page: My Address and My Personal Information data
-#
-#Options
-
+  @store5
+  Scenario: Submit the form and verify the data on My Account page: My Address and My Personal Information data
+    When I click Sign in button
+    When I type "zqarsgiaqkagt@mail.ru" in the email field
+    When I try to create an account
+    When I wait for element with xpath "//button[@id='submitAccount']" to be present
+    When I fill out the form with valid info
+    When I try to submit the form
+    Then element with xpath "//*[@title='Addresses']" should be displayed
+    When I click on `My address` button
+    Then element with xpath "//*[@title='Update']" should be displayed
+    Then I verify info in `My address` tab
+    When I go back to my account
+    When I click on `My personal info` button
+    Then element with xpath "//*[@name='submitIdentity']" should be displayed
+    Then I verify info inside `My personal info` tab
